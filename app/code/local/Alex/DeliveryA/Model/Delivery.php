8c74dbd900f3ed24e1e1dedcbc3bb4c32ed4dc3e
oakhtyrskyi <oleksandr.akhtyrskyi@polcode.net>
2015-04-10 10:37:58 +0200

    100420151037

---
+++ app/code/local/Alex/DeliveryA/Model/Delivery.php
@@ -0,0 +1,9 @@
+<?php
+
+class Alex_DeliveryA_Model_Delivery{
+    
+    public function sayHello(){
+        echo 'hi';
+    }
+}
+?>
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'app/Mage.php';
+
+Mage::app();
+
+$test = new Alex_DeliveryA_Model_Delivery;
+$test->sayHello(); 
+?>
