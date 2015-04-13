7255a8ffe9bd60e5367b76c320158901b36c7238
oakhtyrskyi <oleksandr.akhtyrskyi@polcode.net>
2015-04-13 17:03:28 +0200

    130420151703

--- test.php
+++ test.php
@@ -3,9 +3,20 @@
 require_once 'app/Mage.php';
 
 Mage::app();
-
-$test = new Alex_DeliveryA_Model_Delivery;
+/*
+$test = new TutsPlus_Demo_Model_Product;
 $test->sayHello(); 
+*/
+echo 111;
 
+$customer = Mage::getModel("customer/session");
+
+Mage::getModel("catalog/product");
+
+$product = Mage::getModel("demo/product");
+
+$product->sayHello(); 
+/*
+*/
 ?>
 
