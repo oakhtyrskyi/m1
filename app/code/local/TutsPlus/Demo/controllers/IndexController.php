c36806869a021de9a543384238d7cca78ed7094d
oakhtyrskyi <oleksandr.akhtyrskyi@polcode.net>
2015-04-14 13:40:51 +0200

    140420151340

---
+++ app/code/local/TutsPlus/Demo/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+class TutsPlus_Demo_IndexController 
+extends Mage_Core_Controller_Front_Action {
+   
+   public function sayHelloAction(){
+       
+       echo 'hellow';
+   }
+
+  public function indexAction(){
+       
+       echo 'hello default';
+   }
+  
+  
+?>
--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 require_once 'app/Mage.php';
 
-Mage::app();
+Mage::app('admin');
 /*
 $test = new TutsPlus_Demo_Model_Product;
 $test->sayHello(); 
@@ -21,10 +21,20 @@ $product->sayHello();
 */
 //Mage::getModel("catalog/product");
 
-$category = Mage::getModel("catalog/category")->load(4);
-echo '<pre>';
-var_dump($category->getChildren());
+$pr = Mage::getModel("catalog/product")->getCollection()->addAttributeToSelect(array('name','price'))->addFieldFilter('price'.array("gt"=>50));
 
+foreach($pr as $p){
+    var_dump($p);
+   // die();
+}
 
+//echo '<pre>';
+//echo $pr->delete();
+
+//echo $pr->getMetaTitle();
+/*
+echo $pr->setMetaTitle("new")->save();
+
+echo $pr->getMetaTitle();*/
 ?>
 
